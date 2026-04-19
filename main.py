71ae645ba7d0f55246a2e8e3a84d69c6b7951fb5
SRMIRZA <155520606+SRMIRZA@users.noreply.github.com>
2026-04-19 16:13:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # -------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # This allows GitHub Pages to connect
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
